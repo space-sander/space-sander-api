e3c69e39a54931476133ae61045295e2d3a3e10d
Aayush Patel <aayushpatel48@gmail.com>
2021-04-06 19:56:33 -0400

    added code to rating.cs

---
+++ backend/space.sander.web.Domain/Catalog/Rating.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace space_sander_web.sander.web.Domain.Catalog
+{
+    public class Rating
+    {
+        public int Star { get; set; }
+        public string UserName { get; set; }
+        public string Review { get; set; }
+    }
+}
