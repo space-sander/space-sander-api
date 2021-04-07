4a650f29b1b51469b615ca238f43f0ff2750f6a6
Aayush Patel <aayushpatel48@gmail.com>
2021-04-07 11:22:46 -0400

    finished all the codes for CatalogController

--- backend/space.sander.web.Api/Controllers/CatalogController.cs
+++ backend/space.sander.web.Api/Controllers/CatalogController.cs
@@ -18,6 +18,38 @@ namespace space.sander.web.Api.Controllers
         };
         return Ok(items);
     }
+    [HttpGet("{id:int")]
+    public IActionResult GetItem(int id)
+    {
+        var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+        item.Id = id;
+
+        return Ok(item);
+    }
+    [HttpPost]
+    public IActionResult Post(Item item)
+    {
+        return Created("/catalog/42", item);
+    }
+    [HttpPost("{id:int}/ratings")]
+    public IActionResult PostRating(int id, [FormBody] Rating rating)
+    {
+        var item = new Item("Shirt", "Ohio State Shirt.","Nike",29.99m);
+        item.Id = id;
+        item.AddRating(rating);
+
+        return Ok(item);
+    }
+    [HttpPut("{id:int}")]
+    public IActionResult PutItem(int id, [FromBody] Item item)
+    {
+        return Ok();
+    }
+    [HttpDelete]
+    public IActionResult DeleteItem(int id)
+    {
+        return Ok();
+    }
     }
 
 
