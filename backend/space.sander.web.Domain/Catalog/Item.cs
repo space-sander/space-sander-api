ddcce3ea19da867902c5f8f4810f7a8886688223
Johnathan Margheret <jcmargheret@live.com>
2021-04-07 00:42:54 -0400

    Fixed item and rating

--- backend/space.sander.web.Domain/Catalog/Item.cs
+++ backend/space.sander.web.Domain/Catalog/Item.cs
@@ -8,42 +8,40 @@ namespace space.sander.web.Domain.Catalog
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string brand { get; set; }
+        public string Brand { get; set; }
         public decimal Price { get; set; }
-        public list<Rating> Ratings { get; set; }
+        public List<Rating> Ratings { get; set; }
 
-    }
-
-public void AddRating(Rating rating)
-{
-    this.Ratings.Add(rating);
-}
-public Item(string name, string description, string brand, decimal price)
-{
-    if (string.IsNullOrEmpty(name))
-    { 
-        throw new ArgumentException("Item name cannot be null.");
-    }
-
-    if(string.IsNullOrEmpty(description))
-    { 
-        throw new ArgumentException("Item description cannot be null.");
-    }
-
-    if(string.IsNullOrEmpty(brand))
+    public Item(string name, string description, string brand, decimal price)
     {
-        throw new ArgumentException("Item brand cannot be null.");
-    }
+        if (string.IsNullOrEmpty(name))
+        { 
+            throw new ArgumentException("Item name cannot be null.");
+        }
 
-    if(price <0.00m || price > 1000.00m)
+        if(string.IsNullOrEmpty(description))
+        { 
+            throw new ArgumentException("Item description cannot be null.");
+        }
+
+        if(string.IsNullOrEmpty(brand))
+        {
+            throw new ArgumentException("Item brand cannot be null.");
+        }
+
+        if(price <0.00m || price > 1000.00m)
+        {
+            throw new ArgumentException("Item price must be a positive amount less than $1000.00");
+        }
+
+        this.Name = name;
+        this.Description = description;
+        this.Brand = brand;
+        this.Price = price;
+    }  
+    public void AddRating(Rating rating)
     {
-        throw new ArgumentException("Item price must be a positive amount less than $1000.00");
-    }
-
-    this.Name = name;
-    this.Description = description;
-    this.Brand = brand;
-    this.Price = price;
+        this.Ratings.Add(rating);
+    } 
 }
-
 }
--- backend/space.sander.web.Domain/Catalog/Rating.cs
+++ backend/space.sander.web.Domain/Catalog/Rating.cs
@@ -2,27 +2,28 @@ using System;
 
 namespace space.sander.web.Domain.Catalog
 {
-    public class Rating{
+    public class Rating
+    {
         public int Star { get; set; }
         public string UserName { get; set; }
         public string Review { get; set;}
+
+    public Rating(int stars, string userName, string review)
+    {
+        if (stars < 1 || stars > 5)
+        {
+            throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, or 5.");
+        }
+
+        if(string.IsNullOrEmpty(userName))
+        {
+            throw new ArgumentException("UserName cannot be null.");
+        }
+        this.Star = stars;
+        this.UserName = userName;
+        this.Review = review;
     }
 
-
-public Rating(int stars, string userName, string review)
-{
-    if (stars < 1 \\ stars > 5)
-    {
-        throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, or 5.");
-    }
-
-    if(string.IsNullOrEmpty(userName))
-    {
-        throw new ArgumentException("UserName cannot be null.");
     }
 }
 
-this.Star = stars;
-this.UserName = userName;
-this.Review = review;
-}
