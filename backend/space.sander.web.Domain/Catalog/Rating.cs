9e001b48685ccea2366a0417e8d5096e43615630
Johnathan Margheret <jcmargheret@live.com>
2021-04-06 20:00:05 -0400

    Update Rating.cs

--- backend/space.sander.web.Domain/Catalog/Rating.cs
+++ backend/space.sander.web.Domain/Catalog/Rating.cs
@@ -1,11 +1,27 @@
 using System;
 
-namespace space_sander_web.sander.web.Domain.Catalog
+namespace space.sander.web.Domain.Catalog
 {
-    public class Rating
-    {
+    public class Rating{
         public int Star { get; set; }
         public string UserName { get; set; }
-        public string Review { get; set; }
+        public string Review { get; set;}
     }
 }
+
+public Rating(int stars, string userName, string review)
+{
+    if (stars < 1 \\ stars > 5)
+    {
+        throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, or 5.");
+    }
+
+    if(string.IsNullOrEmpty(userName))
+    {
+        throw new ArgumentException("UserName cannot be null.");
+    }
+}
+
+this.Star = stars;
+this.UserName = userName;
+this.Review = review;
