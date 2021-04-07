0b61fbac21d65ad7dcb87b9f7ce7ff0013159dc7
Johnathan Margheret <jcmargheret@live.com>
2021-04-06 20:00:33 -0400

    Update Item.cs

---
+++ backend/space.sander.web.Domain/Catalog/Item.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace space.sander.web.Domain.Catalog
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string brand { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
