71b1001515392d7b55e0296d57ea319f405c8587
Johnathan Margheret <jcmargheret@live.com>
2021-04-06 23:04:39 -0400

    Update Item.cs - add constructor

--- backend/space.sander.web.Domain/Catalog/Item.cs
+++ backend/space.sander.web.Domain/Catalog/Item.cs
@@ -15,5 +15,28 @@ namespace space.sander.web.Domain.Catalog
 
 public Item(string name, string description, string brand, decimal)
 {
-    
+    if (string.IsNullOrEmpty(name))
+    { 
+        throw new ArgumentException("Item name cannot be null.");
+    }
+
+    if(string.IsNullOrEmpty(description))
+    { 
+        throw new ArgumentException("Item description cannot be null.");
+    }
+
+    if(string.IsNullOrEmpty(brand))
+    {
+        throw new ArgumentException("Item brand cannot be null.");
+    }
+
+    if(price <0.00m || price > 1000.00m)
+    {
+        throw new ArgumentException("Item price must be a positive amount less than $1000.00");
+    }
+
+    this.Name = name;
+    this.Description = description;
+    this.Brand = brand;
+    this.Price = price;
 }
