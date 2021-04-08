e54545a021a1121a793f47d9b3717d5061a496e1
Aayush Patel <aayushpatel48@gmail.com>
2021-04-08 14:28:35 -0400

    project update + Created database context

---
+++ backend/space.sander.web.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using space.sander.web.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace space.sander.web.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {}
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
