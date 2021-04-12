94aba4a92e3a3d9f54ef8054a9bdac2589fe0f86
Aayush Patel <aayushpatel48@gmail.com>
2021-04-12 17:17:06 -0400

    added delete

--- backend/space.sander.web.Api/Controllers/CatalogController.cs
+++ backend/space.sander.web.Api/Controllers/CatalogController.cs
@@ -55,6 +55,15 @@ namespace space.sander.web.Api.Controllers
     [HttpDelete]
     public IActionResult DeleteItem(int id)
     {
+        var item = _db.Items.Find(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        _db.Items.Remove(item);
+        _db.SaveChanges();
+        
         return Ok();
     }
     }
