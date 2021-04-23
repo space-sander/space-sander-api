cc8e0de7b8c9e98382c4363812a30f836e477387
Aayush Patel <aayushpatel48@gmail.com>
2021-04-23 18:37:06 -0400

    added references to fix errors

--- backend/space_sander_web.Domain.Tests/RatingTests.cs
+++ backend/space_sander_web.Domain.Tests/RatingTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using space.sander.web.Domain.Catalog;
 
 namespace space_sander_web.Domain.Tests
 {
@@ -6,11 +8,6 @@ namespace space_sander_web.Domain.Tests
     public class UnitTest1
     {
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Create_With_Bad_Start_Trows_Error()
-        {
-            var rating = new Rating (0, "Name", "Review");
-        }
         public void Can_Create_New_Rating()
         {
             //Arrange
@@ -22,6 +19,12 @@ namespace space_sander_web.Domain.Tests
             Assert.AreEqual(1, rating.Star);
             Assert.AreEqual("Mike", rating.UserName);
             Assert.AreEqual("Great fit!", rating.Review);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Create_With_Bad_Start_Trows_Error()
+        {
+            var rating = new Rating (0, "Name", "Review");
         }
     }
 }
